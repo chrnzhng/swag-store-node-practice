f41dbbd0f37c7132cc047756a30ccfd103cd8738
Jordan Zhang <chrn.zhng@gmail.com>
2017-08-05 13:16:58 -0600

    Created cart endpoints

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const checkForSession = require('./middlewares/checkForSession');
 // Controllers
 const swag_controller = require('./controllers/swag_controller');
 const auth_controller = require( './controllers/auth_controller');
+const cart_controller = require('./controllers/cart_controller');
 
 const app = express();
 
@@ -27,6 +28,11 @@ app.post( '/api/login', auth_controller.login );
 app.post( '/api/register', auth_controller.register );
 app.post( '/api/signout', auth_controller.signout );
 app.get( '/api/user', auth_controller.getUser );
+
+// Cart
+app.post( '/api/cart', cart_controller.add);
+app.post( '/api/cart/checkout', cart_controller.checkout);
+app.delete( '/api/cart', cart_controller.delete);
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
